770712bd0749943c8092394804591949527424ab
= <yhk04@mail.aub.edu>
2014-07-22 17:08:07 -0400

    fix layout

--- app/controllers/matches_controller.rb
+++ app/controllers/matches_controller.rb
@@ -30,7 +30,7 @@ class MatchesController < ApplicationController
     respond_to do |format|
       if @match.save
         format.html { redirect_to @match, notice: 'Match was successfully created.' }
-        format.json { render action: 'show', status: :created, location: @match }
+        format.json { render action: 'show', status: :created, address: @match }
       else
         format.html { render action: 'new' }
         format.json { render json: @match.errors, status: :unprocessable_entity }
